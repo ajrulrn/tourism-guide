7b5fb18f1d55a594948d434daca9c8cb0ecc8065
Ajrul Rizqi <drones920@gmail.com>
2022-05-25 14:12:50 +0700

    rename migration

---
+++ app/migrations/20220522165140_create_table_users.php
@@ -0,0 +1,51 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Create_table_users extends CI_Migration {
+
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'BIGINT',
+                'auto_increment' => TRUE,
+                'unsigned' => TRUE 
+            ),
+            'name' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '150',
+            ),
+            'username' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '16',
+                'unique' => TRUE
+            ),
+            'password' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'email' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+                'unique' => TRUE
+            ),
+            'level_id' => array(
+                'type' => 'INT',
+            ),
+            'is_activate' => array(
+                'type' => 'INT',
+                'constraint' => '1',
+                'default' => 0
+            ),
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('users');
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table('users');
+    }
+
+}
