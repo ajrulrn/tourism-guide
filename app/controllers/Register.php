df7f9023fc60f6cd922503ae64c52dcf684dc7cc
Ajrul Rizqi <drones920@gmail.com>
2022-05-25 14:55:08 +0700

    update path

--- app/controllers/Register.php
+++ app/controllers/Register.php
@@ -6,7 +6,7 @@ class Register extends CI_Controller {
     public function index()
     {
         if ($this->session->has_userdata(SESSION_KEY)) redirect('admin/dashboard');
-        $this->load->view('pages/admin/auth/register');
+        $this->load->view('pages/auth/register');
     }
 
     public function store()
