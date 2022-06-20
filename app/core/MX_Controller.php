1b5743d3e97ee3b73865ba3ac1e800c401a603d0
Ajrul Rizqi <drones920@gmail.com>
2022-06-20 09:47:51 +0700

    change redirect url

--- app/core/MX_Controller.php
+++ app/core/MX_Controller.php
@@ -6,7 +6,7 @@ class MX_Controller extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        if (!$this->session->has_userdata(SESSION_KEY)) redirect('admin/auth');
+        if (!$this->session->has_userdata(SESSION_KEY)) redirect('login');
     }
 
 }
